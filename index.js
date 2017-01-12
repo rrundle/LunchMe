21e6f612c752ce797d580347465b4e5bc0e09ee2
Ryan Rundle <ryan.rundle@ymail.com>
2017-01-11 17:40:28 -0800

    Elongated strict sms syntax creates order in Postmates, #2.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var express = require('express')
 var app = express()
 
 var twilio = require('twilio')
-var twiml = new twilio.TwimlResponse()
 var accountSid = 'AC8531d011f7354e888b1ff5814ede7216'
 var authToken = '25d3020b826467462385f76a53dd9caa'
 var client = require('twilio')(accountSid, authToken)
@@ -16,47 +15,58 @@ var jsonParser = bodyParser.json()
 
 var PORT = 2999
 
-var delivery = {
-  manifest: "",
-  pickup_name: "Pitfire Pizza",
-  pickup_address: "353 East 17th Street, Costa Mesa, CA 92627",
-  pickup_phone_number: "6268266620",
-  dropoff_name: "Ryan",
-  dropoff_address: "1206 Las Arenas Way, Costa Mesa, CA 92627",
-  dropoff_phone_number: "6268402294",
-  dropoff_notes: "Front door on the back side"
+function makeDelivery(manifest, pickup_name) {
+  return {
+    manifest: manifest,
+    pickup_name: pickup_name,
+    pickup_address: "353 East 17th Street, Costa Mesa, CA 92627",
+    pickup_phone_number: "6268266620",
+    dropoff_name: "Ryan",
+    dropoff_address: "1206 Las Arenas Way, Costa Mesa, CA 92627",
+    dropoff_phone_number: "6268402294",
+    dropoff_notes: "Front door on the back side"
+  }
+}
+
+function cantProcess(response, twilio) {
+  twilio.message('Sorry we could not process that order.')
+  response.writeHead(200, {'Content-Type': 'text/xml'})
+  response.end(twilio.toString())
 }
 
 app.use(urlParser)
 app.use(jsonParser)
 
 app.post('/sms', function(req, res) {
-  if (req.body.Body.toLowerCase() === 'pizza') {
-    delivery.manifest = 'a cheese pizza'
+  var textString = req.body.Body
+  var space = ' '
+  var arrayOfText = textString.split(space)
+  if (arrayOfText[1].toLowerCase() !== 'pizza' || arrayOfText[3].toLowerCase() !== 'pitfire') {
+    var twiml = new twilio.TwimlResponse()
+    cantProcess(res, twiml)
+    return
+  }
+  if (arrayOfText[1].toLowerCase() === 'pizza' && arrayOfText[3].toLowerCase() === 'pitfire') {
+    var manifest = 'large cheese pizza'
+    var pickup_name = 'Pitfire Pizza'
+    var delivery = makeDelivery(manifest, pickup_name)
     postmates.new(delivery, function(err, confirm) {
-      console.log(confirm.body)
-      twiml.message('Thanks! We got your order! Your order number is ' + confirm.body.id + '. We\'ll send you a status update shortly.')
+      var twiml = new twilio.TwimlResponse()
+      twiml.message('Thanks! We got your order! Your order number is ' + confirm.body.id + '. Your ' + manifest + ' from ' + pickup_name + ' is on its way!')
       res.writeHead(200, {'Content-Type': 'text/xml'})
       res.end(twiml.toString())
     })
   }
-  if (req.body.Body.toLowerCase() !== 'pizza') {
-    twiml.message('Sorry we could not process that order.')
-    res.writeHead(200, {'Content-Type': 'text/xml'})
-    res.end(twiml.toString())
-  }
 })
 
 app.post('/postmates', function(req, res) {
-  /*
   client.messages.create({
       to: '+16268402294',
       from: '+16266583335',
-      body: 'Status of order #' + req.body.id + ': ' + req.body.data.status,
+      body: 'Status of order #' + req.body.id + ': Your ' + req.body.data.manifest.description + ' from ' + req.body.data.pickup.name + ' is in ' + req.body.data.status,
   }, function(err, message) {
       console.log(message.sid)
   })
-  */
   res.sendStatus(200)
 })
 
