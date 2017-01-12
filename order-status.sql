f98f4dcbc2bbfb2f37044dc8ee0867c075c24cd7
Ryan Rundle <ryan.rundle@ymail.com>
2017-01-12 14:55:22 -0800

    Added order status table, #17.

---
+++ order-status.sql
@@ -0,0 +1,9 @@
+drop table if exists order_status;
+create table order_status (
+  id int,
+  order_id int,
+  status_id serial,
+  sms_update_body varchar,
+  order_status varchar,
+  status_timestamp timestamp
+);
