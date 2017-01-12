3220f19fa38e40d2bbfa9680c4d104c193fd5938
Ryan Rundle <ryan.rundle@ymail.com>
2017-01-12 14:54:34 -0800

    Added orders table, #17.

---
+++ orders.sql
@@ -0,0 +1,11 @@
+drop table if exists orders;
+create table orders (
+  id int,
+  order_id serial,
+  sms_body varchar,
+  order_status varchar,
+  order_price varchar,
+  order_vendor varchar,
+  order_item varchar,
+  order_time timestamp
+);
