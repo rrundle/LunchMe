d362aa88ac75ce8801983c4837e8e0ce7f2838a9
Ryan Rundle <ryan.rundle@ymail.com>
2017-01-12 14:56:06 -0800

    Added emojis table with corresponding restaurant, #17.

---
+++ emojis.sql
@@ -0,0 +1,10 @@
+drop table if exists emojis;
+create table emojis (
+  emoji varchar,
+  value varchar,
+  restaurant varchar,
+  res_address varchar,
+  res_city varchar,
+  res_state varchar,
+  res_zip varchar
+);
